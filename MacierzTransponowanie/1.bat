784875d5986b42f361c21dca125d23b83582e51d
Mateusz "FriZiii" Sawosz <sawoszmateusz@gmail.com>
2022-11-17 23:26:55 +0100

    Macierze Transponowanie added

---
+++ MacierzTransponowanie/1.bat
@@ -0,0 +1,10 @@
+@echo off
+for /f "tokens=1 delims= " %%G in (macierz.txt) do echo|set /p="%%G "
+echo:
+for /f "tokens=2 delims= " %%G in (macierz.txt) do echo|set /p="%%G "
+echo:
+for /f "tokens=3 delims= " %%G in (macierz.txt) do echo|set /p="%%G "
+echo:
+for /f "tokens=4 delims= " %%G in (macierz.txt) do echo|set /p="%%G "
+echo:
+for /f "tokens=5 delims= " %%G in (macierz.txt) do echo|set /p="%%G "
